9dbf1e599e387f20bb69426c308bda67f76ce3e6
Durgarao Y <durgaraoyejjipurapu@gmail.com>
2020-06-02 22:12:53 +0530

    database connection and Base Url Modification

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://36.255.252.196/testenv/semservice/';
+$config['base_url'] = '';
 /*
 |--------------------------------------------------------------------------
 | Index File
